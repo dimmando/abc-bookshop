5a974066d961970f42a1630f917f700ff7779f3d
Dymtro Chesnokov <dimmando@ukr.net>
2024-10-31 14:46:46 +0000

    Add profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from .forms import UserProfileForm
 
-# Create your views here.
+from checkout.models import Order
+
+
+@login_required
+def profile(request):
+    """ Display the user's profile """
+
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile was updated successfully.')
+        else:
+            messages.error(request,
+                        'Profile was not updated. Please check form inputs.'
+                        )
+    else:
+        form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
+    template = 'profiles/profile.html'
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_page': True,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """ Create order history """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+     f'This is a past confirmation for order number {order_number}. '
+     'An email confirmation was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+        'on_page': True,
+    }
+
+    return render(request, template, context)
